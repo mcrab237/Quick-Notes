98dac3cdd15154a3f8d6eca2404d260b38b0886f
mcrab237 <tonybradpit@gmail.com>
2025-05-07 21:08:20 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Landing from "./routes/Landing";
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <div className="min-h-screem bg-slate-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </>
   );
 }
