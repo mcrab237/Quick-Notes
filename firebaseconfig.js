82318b7a66257541085ce2b4bec28b84ef3d7cd0
mcrab237 <tonybradpit@gmail.com>
2025-05-07 20:48:29 -0400

    Added landing page and initial signin and signup

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBce2r8Ya7DXv9gqXoyvm9Ilv_LUSwr4xg",
+  authDomain: "quick-notes-1870b.firebaseapp.com",
+  projectId: "quick-notes-1870b",
+  storageBucket: "quick-notes-1870b.firebasestorage.app",
+  messagingSenderId: "53335185270",
+  appId: "1:53335185270:web:daf63b7f48e0f12dc48c57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/routes/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h1>Signup</h1>;
+}
+
+export default Login;
